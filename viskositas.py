f6678621d22385790f55e11dc323ae01cdb89ba5
Faris Octa <m.faris.octa@gmail.com>
2023-06-13 09:01:38 +0700

    ADD maintenance picture on Viscosity App

--- viskositas.py
+++ viskositas.py
@@ -3,6 +3,7 @@ import pandas as pd
 import time
 import numpy as np
 import logging
+from PIL import Image
 
 def app():
     logging.info('Viskositas Page Started')
@@ -10,6 +11,11 @@ def app():
     st.write('This is the Viscosity Calculator Page.')
 
     st.subheader("We will be available soon✌️")
+
+    image = Image.open('maintenance.jpeg')
+
+    st.image(image)
+    
 
 if __name__ == "__main__":
     # Setup logging configuration
