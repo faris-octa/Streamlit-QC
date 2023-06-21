da1b46ddda0d9d4f27eebfa7b03129a2fd555c26
Faris Octa <m.faris.octa@gmail.com>
2023-06-21 13:44:07 +0700

    ADD functionality of temp table

--- viskositas.py
+++ viskositas.py
@@ -1,13 +1,14 @@
 import streamlit as st
 import pandas as pd
-import time
+from sqlalchemy.sql import text
+from sqlalchemy import create_engine
 import numpy as np
+import time
 import logging
-from PIL import Image
 
 # Database connection string
 conn = st.experimental_connection("qcdb", type="sql", autocommit=True)
-# engine = create_engine("mysql+mysqldb://inkaliqc:qcoke@192.168.0.70/qc")
+engine = create_engine("mysql+mysqldb://inkaliqc:qcoke@192.168.0.70/qc")
 
 # Viscosity factor
 visc_factor_data = {
@@ -28,9 +29,51 @@ def calculate_viscosity(spindle, speed, measurement):
     return viscosity
 
 def app():
-    logging.info('Viskositas Page Started')
+    # logging.info('Viskositas Page Started')
     st.title('Viscosity')
     st.write('This is the Viscosity Calculator Page.')
+
+    try:
+        df = conn.query("SELECT * FROM viscosity_temp")
+        active_df = df[['sec_item_num', 'nama_item', 'LOT']].drop_duplicates()
+    except:
+        None
+
+    #########
+    opsi = st.radio('Pilih sampel', options=[f'sampel aktif ({len(active_df)})', 'sampel baru'], horizontal=True, label_visibility='hidden')
+    if opsi == 'sampel baru':
+        input_values = {'sec_item_num': '', 'nama_item': '', 'LOT': ''}
+    else:  # if opsi == 'sampel aktif'
+        def combine_columns(row):
+            return str(row['nama_item']) + ' (' + str(row['LOT']) + ')'
+
+        combined_list = active_df.apply(combine_columns, axis=1).tolist()
+        selected_sample = st.selectbox('Pilih Sampel', options=combined_list, index= len(active_df)-1)
+        selected_row = df.loc[(df['nama_item'] + ' (' + df['LOT'] + ')') == selected_sample]
+        input_values = {label: selected_row[label].values[0] for label in ['sec_item_num', 'nama_item', 'LOT']}
+
+        displayed_df = df[(df['nama_item'] == input_values['nama_item']) & (df['LOT'] == input_values['LOT'])].sort_values(by='timestamp', ascending=True)
+        st.table(displayed_df)
+
+    #########
+        if st.button("Submit to database"):
+            try:
+                with conn.session as session:
+                    displayed_df.iloc[:, 1:].to_sql('viscosity', con=engine, if_exists='append', index=False)
+                    session.execute(text("DELETE FROM viscosity_temp WHERE nama_item=:n1 AND LOT=:n2"), {"n1": input_values['nama_item'], "n2": input_values['LOT']})
+                    st.success("Data successfully moved from viscosity temp to av in the database.")
+                    st.cache_data.clear()
+                    st.experimental_rerun()
+            except Exception as e:
+                st.error(f"An error occurred: {e}")
+
+
+
+
+
+
+
+
 
 
 
@@ -52,6 +95,18 @@ def app():
             keterangan = st.text_input('keterangan')
         
             submitted = st.button('submit')
+
+        if submitted:
+            print(sec_item_num, nama_item, LOT, spindle, speed, viscosity, keterangan)
+            with conn.session as session:
+                session.execute(text("""INSERT INTO viscosity_test (sec_item_num, nama_item, LOT, spindle, speed, viscosity, keterangan) 
+                                    VALUES (:n1, :n2, :n3, :n4, :n5, :n6, :n7);"""), 
+                                    {"n1":sec_item_num, "n2":nama_item, 
+                                    "n3":LOT, "n4":spindle, "n5": speed,
+                                    "n6": viscosity, "n7":keterangan
+                                    })
+            st.success('Yeay, data has been successfully inserted!')
+            st.experimental_rerun()
     
 
 if __name__ == "__main__":
