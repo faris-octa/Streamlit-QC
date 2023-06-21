78f77f915a41334489e7b4c0aafa038e7fdbcaf0
Faris Octa <m.faris.octa@gmail.com>
2023-06-21 08:23:30 +0700

    ADD viscosity factor dataframe

--- viskositas.py
+++ viskositas.py
@@ -5,6 +5,18 @@ import numpy as np
 import logging
 from PIL import Image
 
+visc_factor = {
+    "speed": [0.3, 0.6, 1.5, 3, 6, 12, 30, 60, 0.3, 0.6, 1.5, 3, 6, 12, 30, 60, 0.3, 0.6, 1.5, 3, 6, 12, 30, 60, 0.3, 0.6, 1.5, 3, 6, 12, 30, 60],
+    "spindle": [1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4],
+    "factor": [200, 100, 40, 20, 10, 5, 2, 1]
+}
+
+visc_factor_df = pd.DataFrame(visc_factor)
+
+# filter data
+result = visc_factor_df[(visc_factor_df['speed'] == 1) & (visc_factor_df['spindle'] == 1)]
+print(result['factor'])
+
 def app():
     logging.info('Viskositas Page Started')
     st.title('Viscosity')
