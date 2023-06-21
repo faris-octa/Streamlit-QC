793901dbeb611e5413df00f78bcf89c7751fb470
Faris Octa <m.faris.octa@gmail.com>
2023-06-21 09:46:38 +0700

    ADD container for viscosity function implementation

--- viskositas.py
+++ viskositas.py
@@ -7,7 +7,7 @@ from PIL import Image
 
 # Database connection string
 conn = st.experimental_connection("qcdb", type="sql", autocommit=True)
-engine = create_engine("mysql+mysqldb://inkaliqc:qcoke@192.168.0.70/qc")
+# engine = create_engine("mysql+mysqldb://inkaliqc:qcoke@192.168.0.70/qc")
 
 # Viscosity factor
 visc_factor_data = {
@@ -18,7 +18,7 @@ visc_factor_data = {
             4000, 2000, 800, 400, 200, 100, 40, 20,
             20000, 10000, 4000, 2000, 1000, 500, 200, 100]
 }
-v_factor_df = pd.DataFrame(visc_factor)
+v_factor_df = pd.DataFrame(visc_factor_data)
 
 # Viscosity calculation
 def calculate_viscosity(spindle, speed, measurement):
@@ -32,11 +32,26 @@ def app():
     st.title('Viscosity')
     st.write('This is the Viscosity Calculator Page.')
 
-    st.subheader("We will be available soon✌️")
 
-    image = Image.open('maintenance.jpeg')
 
-    st.image(image, use_column_width='always')
+    with st.container() as input_section:
+        col1, col2 = st.columns(2)
+
+        with col1:
+            sec_item_num = st.text_input("Second item number")
+            nama_item = st.text_input("Nama item")
+            LOT = st.text_input("LOT")
+
+        with col2:
+            spindle = st.selectbox('tipe spindle', options=(1, 2, 3, 4))
+            speed = st.selectbox('speed', options=(0.3, 0.6, 1.5, 3, 6, 12, 30, 60))
+            measurement = st.text_input("measurement")
+            if measurement:
+                viscosity = calculate_viscosity(spindle, speed, int(measurement))
+                st.text(f"nilai viskositas: {viscosity} mPas")
+            keterangan = st.text_input('keterangan')
+        
+            submitted = st.button('submit')
     
 
 if __name__ == "__main__":
