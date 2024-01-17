492f48571c14f752ed119b48a898fededdcf8719
Faris Octa <m.faris.octa@gmail.com>
2024-01-17 10:42:05 +0700

    Add Total Amine tracker

--- home.py
+++ home.py
@@ -69,4 +69,45 @@ if qc_option == "Acid Value":
         # st.dataframe(df)
 
 elif qc_option == "Total Amine":
-    None
+    df = qc_conn.query("select * from totalamine", 
+    show_spinner = True, 
+    ttl=10
+    )
+
+    sampel_option_ta = st.selectbox(
+        'Nama Item',
+        df['ItemDescription'].unique().tolist(),
+        index=None,
+        placeholder="Pilih Item...",
+        key = "sampel_option_ta"
+        )
+
+    if sampel_option_ta != None:
+        lot_option_ta = st.selectbox(
+            'Lot Number',
+            df.loc[df['ItemDescription'] == sampel_option_ta, 'LotSerialNumber'].unique().tolist(),
+            index=None,
+            placeholder="Pilih Lot...",
+            key = "lot_option_ta"
+            )
+
+        if lot_option_ta is not None:
+            st.dataframe(
+                df.loc[(df['ItemDescription'] == sampel_option_ta) & (df['LotSerialNumber'] == lot_option_ta),
+                ["TimeStamp", "Suhu", "FaktorBuret", "FaktorHClO4", "BeratSampel", "JumlahTitran", "TotalAmine", "Keterangan", "Operator"]
+                ],
+                column_config={
+                    "FaktorBuret": "Faktor Buret",
+                    "FaktorHClO4": "Faktor HClo4",
+                    "BeratSampel": "Berat Sample (g)",
+                    "Suhu": "Temperature",
+                    "JumlahTitran": "Titran (mL)",
+                    "TotalAmine": "Total Amine",
+                    "Keterangan" : "Status",
+                    "TimeStamp" : st.column_config.DatetimeColumn("Waktu", format="h:mm a")
+                },
+                hide_index = True,
+                use_container_width = True
+            )
+        
+            # st.dataframe(df) -> uncomment for debugging
